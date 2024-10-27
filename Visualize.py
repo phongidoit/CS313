91f6b4a6a8dfff3bcc2c23713a67d6cbe576600a
Doan Le Tuan Thanh <thanh99949@gmail.com>
2024-10-27 09:19:29 +0700

    Changes in generating func

--- Visualize.py
+++ Visualize.py
@@ -5,8 +5,8 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 
+from sklearn import datasets
 from sklearn.cluster import AgglomerativeClustering
-
 
 
 warnings.filterwarnings("ignore")
@@ -15,12 +15,43 @@ warnings.filterwarnings("ignore")
 def generate_data(x):
    return np.random.rand(x,2)
 
+def generate_data2(dataset_name, n_samples=50, seed=30):
+    rng = np.random.RandomState(seed)
+    
+    if dataset_name == "noisy_circles":
+        data = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05, random_state=seed)
+        title = "Noisy Circles"
+    elif dataset_name == "noisy_moons":
+        data = datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=seed)
+        title = "Noisy Moons"
+    elif dataset_name == "blobs":
+        data = datasets.make_blobs(n_samples=n_samples, random_state=seed)
+        title = "Blobs"
+    elif dataset_name == "no_structure":
+        data = (rng.rand(n_samples, 2), None)
+        title = "No Structure"
+    elif dataset_name == "aniso":
+        random_state = 170
+        X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+        transformation = [[0.6, -0.6], [-0.4, 0.8]]
+        X_aniso = np.dot(X, transformation)
+        data = (X_aniso, y)
+        title = "Anisotropic Blobs"
+    elif dataset_name == "varied":
+        random_state = 170
+        data = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state)
+        title = "Blobs with Varied Variances"
+    else:
+        raise ValueError("Unknown dataset name.")
+    return np.array(data[0]), np.array(data[1]), title
+
 
 def main():
     st.title("Visualization clustering")
     container = st.container()
     
     col1, col2, col3 = container.columns(3)
+    data = None
     
     with col1:
         optionModel = st.selectbox(
@@ -30,10 +61,11 @@ def main():
         
         
     with col2:
-        numberClusters = st.number_input(
-        "Number of cluster",
-        min_value=0,
-        )
+        # numberClusters = st.number_input(
+        # "Number of cluster",
+        # min_value=0,
+        # )
+        option = st.selectbox("Choose data type?",("noisy_circles", "noisy_moons", "blobs", "no_structure", "aniso", ),)
         
             
     with col3:
@@ -44,7 +76,9 @@ def main():
         
     if container.button("Run", type = 'primary'):
         st.write("Chart here") 
-        data= generate_data(numberDatapoints) 
+        # data= generate_data(numberDatapoints) 
+        data, labels, title = generate_data2(option, numberDatapoints)
+        st.write(data)
         data_pd = pd.DataFrame(data, columns=["x","y"]) 
         st.scatter_chart(data_pd, x="x", y="y")    
     
